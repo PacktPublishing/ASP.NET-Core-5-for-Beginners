05084904676e7693530678c84e34e90457bc8989
Vincent Maverick S Durano <vmsdurano@gmail.com>
2020-07-31 10:55:49 -0500

    [FET] Chpater 4 project samples initial check-in

---
+++ Chapter 04/Chapter_04_RazorViewEngine_Examples/ToDo.MVC/Models/TodoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.MVC.Models
+{
+    public class TodoDbContext: DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
