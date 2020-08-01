5cd5b6e5d78cc2eeb3b43f4f88baf296180767b8
Vincent Maverick S Durano <vmsdurano@gmail.com>
2020-07-31 20:19:29 -0500

    [FET] Added Razor Pages project sample

---
+++ Chapter 04/Chapter_04_RazorViewEngine_Examples/ToDo.RazorPages/Data/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo.RazorPages.Data
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ Chapter 04/Chapter_04_RazorViewEngine_Examples/ToDo.RazorPages/Data/TodoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.RazorPages.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
