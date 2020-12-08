413c8ceead939c5621c6265f0b09baf2a5929277
ahelland <ahelland@users.noreply.github.com>
2020-12-08 21:18:07 +0100

    Initial Chapter 08

---
+++ Chapter 08/Chapter_08_DB_Auth/Chapter_08_DB_Auth/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter_08_DB_Auth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Chapter 08/Chapter_08_DB_Auth/Chapter_08_DB_Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chapter_08_DB_Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
